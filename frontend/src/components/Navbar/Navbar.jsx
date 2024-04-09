1647294767e6c481548dc3db127f5ef4b14c0a0a
Mingtindu <mingmartu@gmail.com>
2024-04-09 21:28:40 +0545

    responsive code adding

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState,useRef } from "react";
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from "react-router-dom";
+import nav_dropdown from '../Assets/dropdown_icon.png'
+
+// import ShopContext from '.../Context/ShopContext.jsx'
 const Navbar = ()=>{
     const [menu,setMenu]= useState("Shop");
+    // const {getTotalCartItems0} = useContext(ShopContext)
+    const menuRef = useRef();
+    const dropdown_toggle=()=>{
+        menuRef.current.classList.toggle('nav-menu-visible')
+        e.target.classList.toggle('open');
+
+    }
     return(
         <div className="navbar">
            <div className="nav-logo">
             <img src={logo} alt="logo" />
             <p>SHOPE ME</p>
            </div>
-           <ul className="nav-menu">
+           <img src={nav_dropdown} onClick={dropdown_toggle} alt="" />
+           <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>{setMenu("Shop")}}><Link style={{ textDecoration:'none'}} to='/'>Shop</Link>{menu=="Shop"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("Men")}}><Link style={{ textDecoration:'none'}} to='/mens'>Men</Link>{menu=="Men"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("Women")}}><Link style={{ textDecoration:'none'}} to='/womens'>Womens</Link> {menu=="Women"?<hr/>:<></>}</li>
