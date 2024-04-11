b9cdf4d05e78df6fb90301f66aeb9398e8ae6a1d
Mingtindu <mingmartu@gmail.com>
2024-04-11 21:07:56 +0545

    feat:admin pannel added

---
+++ admin/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
