85f48d70f655d0282e6871df7c2e5b90b1125ab4
Mingtindu <mingmartu@gmail.com>
2024-04-16 20:49:49 +0545

    feat: access token generate and delete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -30,7 +30,10 @@ const Navbar = ()=>{
 
            </ul>
            <div className="nav-login-cart">
-            <Link to='/login'><button>Login</button></Link>
+            {localStorage.getItem('auth-token')?
+            <button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>:
+            <Link to='/login'><button>Login</button></Link>}
+           
             <Link to='/cart'> <img src={cart_icon} alt="" /></Link>
             <div className="nav-cart-count">
 
